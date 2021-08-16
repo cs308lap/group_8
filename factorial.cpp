95c6bc10feeb9ced614ec841cdd1a25adb8a385f
Bhavna2020 <bhavnakosta2020@gmail.com>
2021-08-16 16:06:41 +0530

    fact

--- factorial.cpp
+++ factorial.cpp
@@ -6,8 +6,8 @@ int factorial(int n){
         cout<<"re-enter a positive number\n";
         return 0;
     }
-    if(n!=1){
-	return(n * factorial(n-1));
-    }
-    else return 1;
+    if(n!=1)
+	   return(n * factorial(n-1));
+    
+    return 1;
 }

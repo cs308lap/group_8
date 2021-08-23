9aa5e763e958b2781e564b70453aec8506986adc
Niveditha N <nniveditha2001@gmail.com>
2021-08-23 13:52:30 +0530

    fix negetive input bug

--- factorial.cpp
+++ factorial.cpp
@@ -4,8 +4,8 @@ int factorial(int n){
     if(n<0)
     {
         //cout<<"re-enter a positive number\n";
-        printf("re-enter a positive number\n");
-        return 0;
+        //printf("re-enter a positive number\n");
+        return -1;
     }
     if(n!=1)
 	   return(n * factorial(n-1));

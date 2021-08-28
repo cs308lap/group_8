82f4fe48dc78689c00a02ed689dc1419f35f9658
Anurag <chauhananurag114@gmail.com>
2021-08-28 23:35:36 +0530

    second commit

--- functions.h
+++ functions.h
@@ -1,2 +1,3 @@
 void print_hello();
 int factorial(int n);
+int getHCF(int x, int y);
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "functions.h"
 using namespace std;
 int main(){
+    int x=56,y=98;
+    cout<<getHCF(x,y)<<endl;
     print_hello();
     cout<< endl;
     cout<<"The factorial of 5 is "<< factorial(5)<< endl;
